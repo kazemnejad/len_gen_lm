c2084a0e814a89289222575abd7853683cccec24
Amirhossein Kazemnejad <2122102+kazemnejad@users.noreply.github.com>
2023-10-06 21:44:13 -0400

    Add new training scripts

--- train_llm.py
+++ train_llm.py
@@ -295,14 +295,10 @@ def main():
     parser = HfArgumentParser(
         (ModelArguments, DataTrainingArguments, TrainingArguments)
     )
-    if len(sys.argv) >= 2 and sys.argv[1].endswith(".json"):
-        # If we pass only one argument to the script and it's the path to a json file,
-        # let's parse it to get our arguments.
-        model_args, data_args, training_args = parser.parse_json_file(
-            json_file=os.path.abspath(sys.argv[1])
-        )
-    else:
-        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    config_path = os.environ["APP_CONFIG_PATH"]
+    model_args, data_args, training_args = parser.parse_json_file(
+        json_file=os.path.abspath(config_path)
+    )
 
     training_args.do_train = True
     training_args.do_eval = True
