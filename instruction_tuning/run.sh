618dcf02637bd03b49001f0416f8cdfb46aea5e4
Amirhossein Kazemnejad <2122102+kazemnejad@users.noreply.github.com>
2023-10-19 17:25:43 -0400

    Fix

--- instruction_tuning/run.sh
+++ instruction_tuning/run.sh
@@ -4,14 +4,14 @@
 export APP_SEED=42
 export APP_DS_SPLIT=$FT_DATASET_SPLIT
 
-export SWEEP_NAME="SW-ft_t5_dec_1b_instruct_tune_pe_${FT_PE_TYPE}_octa___data-octa-instance_len"
+export SWEEP_NAME="SW-ft_t5_dec_1b_instruct_tune_pe_${FT_PE_TYPE}_octa___data-octa-${FT_DATASET_SPLIT}"
 export SWEEP_CONFIGS='configs/sweeps/no_sweep.jsonnet'
 export CAPTURE_LOG=1
 export SWEEP_ROOT_DIR=experiments/$SWEEP_NAME
 export HP_EXP_CONFIG="configs/ft_t5_dec_1b_instruct_tune.jsonnet,configs/models/pe_${FT_PE_TYPE}.jsonnet,configs/data/octa.jsonnet"
 mkdir -p $SWEEP_ROOT_DIR
 
-export WANDB_RUN_GROUP="SW-ft_t5_dec_1b_instruct_tune_pe_${FT_PE_TYPE}_octa___data-octa-instance_len"
+export WANDB_RUN_GROUP="SW-ft_t5_dec_1b_instruct_tune_pe_${FT_PE_TYPE}_octa___data-octa-${FT_DATASET_SPLIT}"
 export WANDB_TAGS=sweep,manual_sweep,launched_by_ngc,instruction_tune,instruction_tune_octa
 
 chmod a+x scripts/manual_sweep_agent.sh
